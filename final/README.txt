3c8ca550d6dd18475f676870a404a1f5482fcd67
Leo <r06942082@ntu.edu.tw>
2018-01-22 18:01:45 +0800

    Final comfirmed upload God bless everyone

--- final/README.txt
+++ final/README.txt
@@ -11,7 +11,7 @@ gensim		3.0.1
 
 ************    run    **************
 >>>cd src
->>>python3 final_predict.py $1 $2 $3
+>>>bash final_predict.sh $1 $2 $3
 
 $1:test.data
 $2:test.csv
--- final/src/final_predict.sh
+++ final/src/final_predict.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-wget https://www.dropbox.com/sh/y1qr9jkp74m9lm2/AADbBssbTv7sbL4sHBaPjS7ua?dl=1 -O lib/w2v_model
+mkdir download 
+wget https://www.dropbox.com/sh/y1qr9jkp74m9lm2/AADbBssbTv7sbL4sHBaPjS7ua?dl=1 -O download/w2v_model
+mkdir lib/w2v_model
 unzip download/w2v_model -d lib/w2v_model
 python final_predict.py $1 $2 $3
